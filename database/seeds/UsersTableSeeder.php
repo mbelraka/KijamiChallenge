2a089d42dc87853724d5c09477c5e95e94ccc5b2
mbelraka <mohamed.elrakayby@gmail.com>
2017-06-19 15:20:24 +0200

    adding user seeding

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@website.com',
+            'password' => bcrypt('admin'),
+            'access_level' => 1,
+        ]);
+    }
+}
