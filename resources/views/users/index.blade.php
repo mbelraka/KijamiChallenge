dbf7ee4b6563f9e77a436ff6888ef182de9b114a
mbelraka <mohamed.elrakayby@gmail.com>
2017-06-20 07:20:25 +0200

    updating index

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,8 +4,8 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">
-                    Clients
+                <div class="panel-heading text-center">
+                    <h4>Clients</h4>
                 </div>
                 <div class="panel-body">
                     <div class="table-responsive">
@@ -22,6 +22,7 @@
                             @endforeach
                         </table>
                     </div>
+                    <a class="btn col-md-4 btn-primary col-md-offset-4" href="{{URL::to('users', 'create')}}">Add Client</a>
                 </div>
             </div>
         </div>
